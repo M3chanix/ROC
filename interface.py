808e38ab6687fb1b2fcc4d4b3824fac8cd9ca440
M3chanix <alexei_katitski@mail.ru>
2019-06-24 06:18:19 +0300

    V1.0

--- interface.py
+++ interface.py
@@ -7,25 +7,28 @@ import matplotlib.pyplot as plt
 from sklearn import metrics
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QAction, QFileDialog, QComboBox, QPushButton, \
-    QGridLayout, QLabel, QTableWidget, QTableWidgetItem, QLineEdit, QInputDialog, QDialog, QCheckBox
+    QGridLayout, QLabel, QTableWidget, QTableWidgetItem, QLineEdit, QInputDialog, QDialog, QCheckBox, QHBoxLayout
 os.chdir("C://py1")
 
 
 def parse_file(fname):
-    file = pd.read_excel(fname, skiprows=19)
-    unique_ids = file["Well Note"].unique()
+    file = pd.read_excel(fname, skiprows=0)
+    file = file.dropna(axis=0, how='all')
+    unique_ids = file["Sample"].unique()
 
     # todo: в реальных данных индексы не должны совподать, поэтому строчка с дублированием лишняя
-    unique_ids = np.append(unique_ids, unique_ids)
+    # unique_ids = np.append(unique_ids, unique_ids)
 
     Cq = file["Cq"].to_numpy()
-    Cq.resize(len(unique_ids), int(len(file)/len(unique_ids)))
-    labels = file["Sample"].unique()
+    miRNA_count = int(len(file)/len(unique_ids))
+    Cq.resize(len(unique_ids), miRNA_count)
+    labels = file["miRNA"].unique()
     patient_table = pd.DataFrame(Cq, columns=labels)
-    patient_table.insert(0, "Sample ID", unique_ids)
+    patient_table.insert(0, "Sample", unique_ids)
     tissue = np.array([])
     for i in range(int(len(unique_ids))):
-        tissue = np.append(tissue, file["Target"].loc[12*i])
+        # todo: сделать обработку случайного числа микрорнк, а не 24
+        tissue = np.append(tissue, file["Tissue"].iloc[miRNA_count*i])
 
     return(unique_ids, tissue, patient_table)
 
@@ -34,6 +37,123 @@ def save_to_sql(data):
     connection = sql.connect("C:/py1/db1.db")
     data.to_sql("Patient_data", connection, if_exists="append", index=False)
     connection.close()
+
+
+def read_from_sql(script):
+    connection = sql.connect("C:/py1/db1.db")
+    sql_data = pd.read_sql(script, connection)
+    connection.close()
+
+    return sql_data
+
+
+class SavedDataWindow(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def get_sql_lables(self):
+        self.sql_lable_values = {}
+        connection = sql.connect("C:/py1/db1.db")
+        cursor = connection.cursor()
+        query_part1 = "select distinct "
+        query_part2 = " from Patient_data"
+        for label in self.sql_labels:
+            query = query_part1+label+query_part2
+            cursor.execute(query)
+            self.sql_lable_values[label] = cursor.fetchall()
+
+        cursor.close()
+        connection.close()
+
+    def getSearcResults(self):
+        script = """
+        Select * From Patient_data
+        """
+
+        queryValues = []
+        for i in (0, 7, 8):
+            current_value = self.searchWidget.cellWidget(0, i).text()
+            if current_value != "":
+                queryValues.append('{} = "{}"'.format(self.table_labels[i], current_value))
+
+        for i in range(1, 7, 1):
+            current_value = self.searchWidget.cellWidget(0, i).currentText()
+            if current_value != "Any":
+                queryValues.append('{} = "{}"'.format(self.table_labels[i], current_value))
+
+        if len(queryValues) > 0:
+            script += " where"
+            for value in queryValues:
+                script += " "
+                script += value
+                script += " and"
+            script = script[:-3]
+
+        self.sql_data = read_from_sql(script)
+        self.resultWidget.setRowCount(len(self.sql_data))
+
+        for i in range(len(self.sql_data)):
+            for j in range(len(self.table_labels)):
+                a = QLabel()
+                a.setText(self.sql_data.iloc[i, j])
+                self.resultWidget.setCellWidget(i, j, a)
+        self.resultWidget.resizeColumnsToContents()
+        self.sql_data = self.sql_data.dropna(axis=1, how="all")
+
+    def prepareAnalyzis(self):
+        results_widget = ResultsWindow()
+        results_widget.initUI(self.sql_data)
+        main_window.setCentralWidget(results_widget)
+
+    def createSearchWidget(self):
+        self.table_labels = ["Sample", "Tissue", "BIN_Diagnosis", "Source", "Material", "Operator_RNA_Isolation",
+                             "Operator_PCR", "RNA_Concentration", "Diagnosis"]
+        self.sql_labels = ["Tissue", "BIN_Diagnosis", "Source", "Material", "Operator_RNA_Isolation",
+                             "Operator_PCR"]
+        self.searchWidget.setColumnCount(len(self.table_labels))
+        self.resultWidget.setColumnCount(len(self.table_labels))
+        self.searchWidget.setHorizontalHeaderLabels(self.table_labels)
+        self.resultWidget.setHorizontalHeaderLabels(self.table_labels)
+        self.get_sql_lables()
+        self.searchWidget.setRowCount(1)
+
+        for key, j in zip(self.sql_labels, range(len(self.sql_labels))):
+            a = QComboBox()
+            a.addItem("Any")
+            for label in self.sql_lable_values[key]:
+                a.addItem(label[0])
+            self.searchWidget.setCellWidget(0, j+1, a)
+
+        for i in (0, 7, 8):
+            a = QLineEdit()
+            self.searchWidget.setCellWidget(0, i, a)
+        self.searchWidget.resizeColumnsToContents()
+
+
+    def initUI(self):
+        self.setWindowTitle("TableWidget Test")
+        self.grid_layout = QGridLayout()
+        self.searchWidget = QTableWidget()
+        self.resultWidget = QTableWidget()
+
+        self.TextLabel = QLabel("Поиск по базе")
+        processSearchButton = QPushButton("Обработать")
+        AnalyzeButton = QPushButton("Провести анализ")
+        self.createSearchWidget()
+        self.grid_layout.addWidget(self.TextLabel)
+        self.grid_layout.addWidget(self.searchWidget)
+        self.grid_layout.addWidget(processSearchButton)
+        self.grid_layout.addWidget(self.resultWidget)
+        self.grid_layout.addWidget(AnalyzeButton)
+
+        processSearchButton.clicked.connect(self.getSearcResults)
+        AnalyzeButton.clicked.connect(self.prepareAnalyzis)
+
+        self.setLayout(self.grid_layout)
+        self.setGeometry(300, 300, 930, 400)
+        self.show()
 
 
 class NewDataWindow(QWidget):
@@ -138,13 +258,36 @@ class NewDataWindow(QWidget):
 
         for i, label in zip(range(len(additional_data.columns)), additional_data.columns):
             self.patient_table.insert(i+1, label, additional_data[label])
-            if i == 0:
-                results_widget.initUI(self.patient_table)
+            # if i == 0:
+            #     results_widget.initUI(self.patient_table)
         self.patient_table.insert(1, "Tissue", self.tissue_list)
-        # todo: перед сохранением в базу дополнить до всех именований miRNA
-        # todo: просто написать запрос с использованием insert into table_name column_list values()
-        # save_to_sql(self.patient_table)
+        results_widget.initUI(self.patient_table)
+        save_to_sql(self.patient_table)
         main_window.setCentralWidget(results_widget)
+
+
+class FirstWindow(QWidget):
+    def NewDataClicked(self):
+        new_data_window = NewDataWindow()
+        main_window.setCentralWidget(new_data_window)
+
+    def SavedDataClicked(self):
+        saved_data_window = SavedDataWindow()
+        main_window.setCentralWidget(saved_data_window)
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.hboxLayout = QHBoxLayout()
+        SavedDataButton = QPushButton("Использовать данные из базы")
+        NewDataButton = QPushButton("Внести новые данные из файла")
+        self.hboxLayout.addWidget(SavedDataButton)
+        self.hboxLayout.addWidget(NewDataButton)
+        NewDataButton.clicked.connect(self.NewDataClicked)
+        SavedDataButton.clicked.connect(self.SavedDataClicked)
+        self.setLayout(self.hboxLayout)
 
 
 class MainWindow(QMainWindow):
@@ -155,9 +298,10 @@ class MainWindow(QMainWindow):
 
     def initUI(self):
         self.setWindowTitle("MainWindow")
-        self.setGeometry(300, 200, 900, 450)
-        new_data = NewDataWindow()
-        self.setCentralWidget(new_data)
+        self.setGeometry(300, 200, 1030, 450)
+        first_window = FirstWindow()
+        # new_data_window = NewDataWindow()
+        self.setCentralWidget(first_window)
         self.show()
 
 
@@ -198,13 +342,10 @@ class ResultsWindow(QWidget):
         self.draw_roc_curve(self.fpr, self.tpr, self.roc_auc, picked_values)
 
     def normalization(self, raw_data):
-        # todo: хз, че делать с NAN
-        # raw_data = raw_data.dropna(axis=0)
-
         column_names = list(raw_data)
-        norm_data = pd.DataFrame(raw_data, columns=raw_data.columns[:2])
-        for divident in column_names[2:]:
-            for divider in column_names[2:]:
+        norm_data = pd.DataFrame(raw_data, columns=raw_data.columns[:9])
+        for divident in column_names[9:]:
+            for divider in column_names[9:]:
                 if divident != divider:
                     norm_data[divident + '/' + divider] = raw_data[divident] / raw_data[divider]
         norm_data = norm_data.fillna(0)
@@ -215,7 +356,7 @@ class ResultsWindow(QWidget):
         tpr = dict()
         threshold = dict()
         roc_auc = dict()
-        for i in norm_data.columns[2:]:
+        for i in norm_data.columns[9:]:
             fpr[i], tpr[i], threshold[i] = metrics.roc_curve(norm_data["BIN_Diagnosis"], norm_data[i], pos_label="HSIL")
             roc_auc[i] = metrics.auc(fpr[i], tpr[i])
 
