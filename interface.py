66e622a628c2527319f4d7fb815e59e376e81888
Jouni Sunnari <dz4tune@gmail.com>
2019-12-22 02:10:31 +0300

    wrote add_divs function

--- interface.py
+++ interface.py
@@ -434,6 +434,7 @@ class ResultsWindow(QWidget):
 
     @staticmethod
     def normalization(raw_data: pandas.DataFrame) -> pandas.DataFrame:
+        # TODO remove 11 from here
         raw_data = raw_data.dropna(axis=1, how="all")
         column_names = list(raw_data)
         norm_data = pandas.DataFrame(raw_data, columns=raw_data.columns[:11])
@@ -467,6 +468,27 @@ class ResultsWindow(QWidget):
         plt.subplots_adjust(right=0.6)
         fig.set_size_inches(10, 5)
         plt.show()
+
+
+def all_divs(data: numpy.ndarray) -> numpy.ndarray:
+    '''
+    >>> from numpy import array
+    >>> arg = array([[1, 2, 8],   \
+                     [3, 4, 6],   \
+                     [5, 6, 8]])
+    >>> all_divs(arg) == array([[1/2, 1/8, 2/1, 2/8, 8/1, 8/2],  \
+                                [3/4, 3/6, 4/3, 4/6, 6/3, 6/4],  \
+                                [5/6, 5/8, 6/5, 6/8, 8/5, 8/6]])
+    array([[ True,  True,  True,  True,  True,  True],
+           [ True,  True,  True,  True,  True,  True],
+           [ True,  True,  True,  True,  True,  True]])
+    '''
+    row_count, col_count = data.shape
+    permutations_count = col_count * (col_count - 1)
+    result = numpy.zeros((row_count, permutations_count))
+    for i, (divident, divider) in enumerate(permutations(range(len(data)), 2)):
+        result[:,i] = data[:,divident] / data[:,divider]
+    return result
 
 
 if __name__ == '__main__':
