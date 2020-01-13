96e95e5774aee435f65f9e364777e9f9ccbf9969
Jouni Sunnari <dz4tune@gmail.com>
2020-01-13 19:04:46 +0300

    moved core funtionality into its own file

---
+++ core.py
@@ -0,0 +1,54 @@
+import numpy
+import pandas
+from sklearn import metrics
+from itertools import permutations
+
+from typing import Dict
+
+class ROC_curve_data:
+
+    def __init__(self, fpr: numpy.ndarray, tpr: numpy.ndarray, threshold: numpy.ndarray, auc: float):
+        self.fpr : numpy.ndarray = fpr
+        self.tpr : numpy.ndarray = tpr
+        self.threshold : numpy.ndarray = threshold
+        self.auc : float = auc
+
+def normalization(raw_data: pandas.DataFrame) -> pandas.DataFrame:
+    # TODO remove 11 from here
+    raw_data = raw_data.dropna(axis=1, how="all")
+    column_names = list(raw_data)
+    norm_data = pandas.DataFrame(raw_data, columns=raw_data.columns[:11])
+    result = all_divs(numpy.array(raw_data.iloc[:,11:]))
+    result_df = pandas.DataFrame(result, columns=map(lambda pair: '{}/{}'.format(*pair), permutations(column_names[11:], 2)), index=norm_data.index)
+    norm_data = pandas.concat([norm_data, result_df], axis='columns')
+    norm_data = norm_data.dropna(axis=1, how="all")
+    norm_data = norm_data.fillna(0)
+    return norm_data
+
+def roc_analyze(norm_data: pandas.DataFrame) -> Dict[str, ROC_curve_data]:
+    result = {}
+    for i in norm_data.columns[11:]:
+        fpr, tpr, threshold = metrics.roc_curve(norm_data["Class"], norm_data[i])
+        roc_auc = metrics.auc(fpr, tpr)
+        result[i] = ROC_curve_data(fpr=fpr, tpr=tpr, threshold=threshold, auc=roc_auc)
+    return result
+
+def all_divs(data: numpy.ndarray) -> numpy.ndarray:
+    '''
+    >>> from numpy import array
+    >>> arg = array([[1, 2, 8],   \
+                     [3, 4, 6],   \
+                     [5, 6, 8]])
+    >>> all_divs(arg) == array([[1/2, 1/8, 2/1, 2/8, 8/1, 8/2],  \
+                                [3/4, 3/6, 4/3, 4/6, 6/3, 6/4],  \
+                                [5/6, 5/8, 6/5, 6/8, 8/5, 8/6]])
+    array([[ True,  True,  True,  True,  True,  True],
+           [ True,  True,  True,  True,  True,  True],
+           [ True,  True,  True,  True,  True,  True]])
+    '''
+    row_count, col_count = data.shape
+    permutations_count = col_count * (col_count - 1)
+    result = numpy.zeros((row_count, permutations_count))
+    for i, (divident, divider) in enumerate(permutations(range(col_count), 2)):
+        result[:,i] = data[:,divident] / data[:,divider]
+    return result
--- interface.py
+++ interface.py
@@ -21,9 +21,8 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QAction, QFileDi
 
 import numpy
 import pandas
-from sklearn import metrics
-from itertools import permutations
 
+from core import normalization, roc_analyze, ROC_curve_data
 from model import Sample
 from parser import parse_file
 
@@ -386,15 +385,6 @@ class MainWindow(QMainWindow):
         self.show()
 
 
-class ROC_curve_data:
-
-    def __init__(self, fpr: numpy.ndarray, tpr: numpy.ndarray, threshold: numpy.ndarray, auc: float):
-        self.fpr : numpy.ndarray = fpr
-        self.tpr : numpy.ndarray = tpr
-        self.threshold : numpy.ndarray = threshold
-        self.auc : float = auc
-
-
 class ResultsWindow(QWidget):
 
     def __init__(self, raw_data: pandas.DataFrame):
@@ -403,8 +393,8 @@ class ResultsWindow(QWidget):
 
     def initUI(self, raw_data: pandas.DataFrame):
         start = time.process_time()
-        norm_data = self.normalization(raw_data)
-        self.roc_data = self.roc_analyze(norm_data)
+        norm_data = normalization(raw_data)
+        self.roc_data = roc_analyze(norm_data)
         print(time.process_time() - start)
 
         layout = QGridLayout()
@@ -433,28 +423,6 @@ class ResultsWindow(QWidget):
         self.draw_roc_curve(self.roc_data, picked_values)
 
     @staticmethod
-    def normalization(raw_data: pandas.DataFrame) -> pandas.DataFrame:
-        # TODO remove 11 from here
-        raw_data = raw_data.dropna(axis=1, how="all")
-        column_names = list(raw_data)
-        norm_data = pandas.DataFrame(raw_data, columns=raw_data.columns[:11])
-        result = all_divs(numpy.array(raw_data.iloc[:,11:]))
-        result_df = pandas.DataFrame(result, columns=map(lambda pair: '{}/{}'.format(*pair), permutations(column_names[11:], 2)), index=norm_data.index)
-        norm_data = pandas.concat([norm_data, result_df], axis='columns')
-        norm_data = norm_data.dropna(axis=1, how="all")
-        norm_data = norm_data.fillna(0)
-        return norm_data
-
-    @staticmethod
-    def roc_analyze(norm_data: pandas.DataFrame) -> Dict[str, ROC_curve_data]:
-        result = {}
-        for i in norm_data.columns[11:]:
-            fpr, tpr, threshold = metrics.roc_curve(norm_data["Class"], norm_data[i])
-            roc_auc = metrics.auc(fpr, tpr)
-            result[i] = ROC_curve_data(fpr=fpr, tpr=tpr, threshold=threshold, auc=roc_auc)
-        return result
-
-    @staticmethod
     def draw_roc_curve(roc_data: Dict[str, ROC_curve_data], miRNA_names):
         fig = plt.figure()
         for name in miRNA_names:
@@ -471,25 +439,7 @@ class ResultsWindow(QWidget):
         plt.show()
 
 
-def all_divs(data: numpy.ndarray) -> numpy.ndarray:
-    '''
-    >>> from numpy import array
-    >>> arg = array([[1, 2, 8],   \
-                     [3, 4, 6],   \
-                     [5, 6, 8]])
-    >>> all_divs(arg) == array([[1/2, 1/8, 2/1, 2/8, 8/1, 8/2],  \
-                                [3/4, 3/6, 4/3, 4/6, 6/3, 6/4],  \
-                                [5/6, 5/8, 6/5, 6/8, 8/5, 8/6]])
-    array([[ True,  True,  True,  True,  True,  True],
-           [ True,  True,  True,  True,  True,  True],
-           [ True,  True,  True,  True,  True,  True]])
-    '''
-    row_count, col_count = data.shape
-    permutations_count = col_count * (col_count - 1)
-    result = numpy.zeros((row_count, permutations_count))
-    for i, (divident, divider) in enumerate(permutations(range(col_count), 2)):
-        result[:,i] = data[:,divident] / data[:,divider]
-    return result
+
 
 
 if __name__ == '__main__':
